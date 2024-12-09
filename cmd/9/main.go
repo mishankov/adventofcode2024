26fcfbef09c0e04f088e55318e58f9c13a08ca41
Denis Mishankov <mishankov@mail.com>
2024-12-09 12:54:28 +0300

    9.2 (not 6)

--- cmd/9/main.go
+++ cmd/9/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc2024/pkg/aocutils"
 	"log"
+	"slices"
 )
 
 type layoutKind int
@@ -18,6 +19,48 @@ type layoutItem struct {
 	id   int
 }
 
+func countChecksum(input []int) int {
+	r := 0
+	for i, item := range input {
+		if item < 0 {
+			break
+		}
+
+		r += i * item
+	}
+
+	return r
+}
+
+func countChecksum2(input []int) int {
+	r := 0
+	for i, item := range input {
+		if item < 0 {
+			continue
+		}
+
+		r += i * item
+	}
+
+	return r
+}
+
+func layoutItemsToIntSlice(lis []layoutItem) ([]int, []int) {
+	layoutInt := []int{}
+	emptyPositions := []int{}
+
+	for _, item := range lis {
+		for range item.size {
+			layoutInt = append(layoutInt, item.id)
+			if item.kind == empty {
+				emptyPositions = append(emptyPositions, len(layoutInt)-1)
+			}
+		}
+	}
+
+	return layoutInt, emptyPositions
+}
+
 func solve(data []byte) (int, int) {
 	layout := []layoutItem{}
 	for index, digitByte := range data {
@@ -29,17 +72,7 @@ func solve(data []byte) (int, int) {
 		}
 	}
 
-	layoutInt := []int{}
-	emptyPositions := []int{}
-
-	for _, item := range layout {
-		for range item.size {
-			layoutInt = append(layoutInt, item.id)
-			if item.kind == empty {
-				emptyPositions = append(emptyPositions, len(layoutInt)-1)
-			}
-		}
-	}
+	layoutInt, emptyPositions := layoutItemsToIntSlice(layout)
 
 	itemPos := len(layoutInt) - 1
 	posIndex := 0
@@ -74,16 +107,46 @@ func solve(data []byte) (int, int) {
 		}
 	}
 
-	r1 := 0
-	for i, item := range layoutInt {
-		if item < 0 {
+	r1 := countChecksum(layoutInt)
+
+	itemPos = len(layout) - 1
+	itemPosDecrement := 0
+	for itemPos >= 0 {
+		item := layout[itemPos]
+		if item.kind != file {
+			itemPosDecrement += 1
+			itemPos = len(layout) - 1 - itemPosDecrement
+			continue
+		}
+
+		for emptyPos, emptyItem := range layout {
+			if emptyItem.kind != empty {
+				continue
+			}
+
+			if emptyPos >= itemPos {
+				continue
+			}
+
+			if emptyItem.size < item.size {
+				continue
+			}
+
+			layout[emptyPos] = item
+			layout = slices.Insert(layout, emptyPos+1, layoutItem{kind: empty, size: emptyItem.size - item.size, id: -1})
+			layout[itemPos+1] = layoutItem{kind: empty, size: item.size, id: -1}
+
 			break
 		}
 
-		r1 += i * item
+		itemPosDecrement += 1
+		itemPos = len(layout) - 1 - itemPosDecrement
 	}
 
-	return r1, 0
+	layoutInt, _ = layoutItemsToIntSlice(layout)
+	r2 := countChecksum2(layoutInt)
+
+	return r1, r2
 }
 
 func main() {
